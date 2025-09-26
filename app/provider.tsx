5936873ebf32d43666415571636055b18ac43d23
joshua takyi <takyijoshua019@gmail.com>
2025-09-26 17:25:59 +0000

    first commit for the client side p

---
+++ app/provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { HeroUIProvider } from "@heroui/react";
+import type { ThemeProviderProps } from "next-themes";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/navigation";
+import * as React from "react";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </HeroUIProvider>
+  );
+}
